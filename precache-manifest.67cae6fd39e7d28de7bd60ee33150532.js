43621393dd0a56c86de9ecc1ac59b7cbc33ee599
Jacob Ingalls <jingalls@uoregon.edu>
2019-04-16 16:12:47 -0700

    Updates

--- precache-manifest.67cae6fd39e7d28de7bd60ee33150532.js
+++ precache-manifest.67cae6fd39e7d28de7bd60ee33150532.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/avengers/static/js/runtime~main.8e2176cc.js"
   },
   {
-    "revision": "34af08f27940a0b3cb43",
-    "url": "/avengers/static/js/main.8510945a.chunk.js"
+    "revision": "f95042bdec8472faa9c9",
+    "url": "/avengers/static/js/main.d0471892.chunk.js"
   },
   {
     "revision": "8795d4af043ddfe37042",
     "url": "/avengers/static/js/2.7cb9f535.chunk.js"
   },
   {
-    "revision": "34af08f27940a0b3cb43",
+    "revision": "f95042bdec8472faa9c9",
     "url": "/avengers/static/css/main.065839d4.chunk.css"
   },
   {
-    "revision": "598e0562d2c753fa20f4936663f50f36",
+    "revision": "910793843a3526478b97952e28151e71",
     "url": "/avengers/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/avengers/precache-manifest.9663ec6c57a5a31c85ee89a69e04318d.js"
+  "/avengers/precache-manifest.67cae6fd39e7d28de7bd60ee33150532.js"
 );
 
 workbox.clientsClaim();
