4f23fc0af5059cd30e30602eb73628e3ed04da84
Jacob Ingalls <jingalls@uoregon.edu>
2019-04-16 16:08:05 -0700

    Updates

---
+++ precache-manifest.9663ec6c57a5a31c85ee89a69e04318d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0cd6398c36d659e10ff",
+    "url": "/avengers/static/js/runtime~main.8e2176cc.js"
+  },
+  {
+    "revision": "34af08f27940a0b3cb43",
+    "url": "/avengers/static/js/main.8510945a.chunk.js"
+  },
+  {
+    "revision": "8795d4af043ddfe37042",
+    "url": "/avengers/static/js/2.7cb9f535.chunk.js"
+  },
+  {
+    "revision": "34af08f27940a0b3cb43",
+    "url": "/avengers/static/css/main.065839d4.chunk.css"
+  },
+  {
+    "revision": "598e0562d2c753fa20f4936663f50f36",
+    "url": "/avengers/index.html"
+  }
+];
